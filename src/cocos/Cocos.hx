d4a97581b5c182667a68b49ff67aec8a3fe1bba2
MaiHD <maihd.dev@gmail.com>
2020-08-17 20:34:21 +0700

    point

--- src/cocos/Cocos.hx
+++ src/cocos/Cocos.hx
@@ -1,6 +1,7 @@
 package cocos;
 
 import cocos.actions.*;
+import cocos.math.*;
 
 @:native("cc")
 extern class Cocos {
@@ -14,6 +15,9 @@ extern class Cocos {
     @:overload(function(a1 : Action, a2 : Action, a3 : Action, a4 : Action) : Void {})
     @:overload(function(a1 : Action, a2 : Action, a3 : Action, a4 : Action, a5 : Action) : Void {})
     public static function sequence(actions : Array<Action>) : Sequence;
+
+    @:native("p")
+    public static function point(x : Float, y : Float, z : Float = 0.0) : Point;
 
     #if html5
 
---
+++ src/cocos/math/Point.hx
@@ -0,0 +1,7 @@
+package cocos.math;
+
+interface Point {
+    var x(default, default) : Float;
+    var y(default, default) : Float;
+    var z(default, default) : Float;
+}
