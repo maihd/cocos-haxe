58c3269055558e3053f954fc99db919cf1de8989
maihd <maihd.dev@gmail.com>
2020-08-18 12:40:31 +0700

    rect & size

---
+++ src/cocos/math/Rect.hx
@@ -0,0 +1,8 @@
+package cocos.math;
+
+interface Rect {
+    var x(default, default) : Float;
+    var y(default, default) : Float;
+    var width(default, default) : Float;
+    var height(default, default) : Float;
+}
---
+++ src/cocos/math/Size.hx
@@ -0,0 +1,6 @@
+package cocos.math;
+
+interface Size {
+    var width(get, set) : Float;
+    var height(get, set) : Float;
+}
