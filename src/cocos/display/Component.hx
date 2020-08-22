7c34aa8b9144c2bc4b9ab32658a655c49985d735
MaiHD <maihd.dev@gmail.com>
2020-08-22 20:20:11 +0700

    cc.Component

--- src/cocos/display/Component.hx
+++ src/cocos/display/Component.hx
@@ -2,5 +2,28 @@ package cocos.display;
 
 @:native("cc.Component")
 extern class Component {
-    
+    @:selfCall
+    public function new();
+
+    public function getName() : String;
+
+    public function getOwner() : Any;
+
+    public function init() : Bool;
+
+    public function isEnabled() : Bool;
+
+    public function onEnter() : Void;
+
+    public function onExit() : Void;
+
+    public function serialize(reader : Any) : Void;
+
+    public function setEnabled(enable : Bool) : Void;
+
+    public function setName(name : String) : Void;
+
+    public function setOwner(owner : Any) : Void;
+
+    public function update(dt : Float) : Void;
 }
