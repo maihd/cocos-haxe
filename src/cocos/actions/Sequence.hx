be27f27d28a53fc3f0db310375bb84d3979866f3
MaiHD <maihd.dev@gmail.com>
2020-08-31 20:59:28 +0700

    cc.Sequence

--- src/cocos/actions/Sequence.hx
+++ src/cocos/actions/Sequence.hx
@@ -1,11 +1,14 @@
 package cocos.actions;
 
 @:native("cc.Sequence")
-extern class Sequence extends Action {
+extern class Sequence extends ActionInterval {
     @:selfCall
     @:overload(function(a1 : Action, a2 : Action) : Void {})
     @:overload(function(a1 : Action, a2 : Action, a3 : Action) : Void {})
     @:overload(function(a1 : Action, a2 : Action, a3 : Action, a4 : Action) : Void {})
     @:overload(function(a1 : Action, a2 : Action, a3 : Action, a4 : Action, a5 : Action) : Void {})
     public function new(actions : Array<Action>);
+
+    public function clone() : Sequence;
+    public function reverse() : Sequence;
 }
