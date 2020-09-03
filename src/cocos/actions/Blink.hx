2ef9537afc9fed48eaf00928e4d364d9c5768a47
MaiHD <maihd.dev@gmail.com>
2020-09-03 21:51:33 +0700

    cc.Blink

---
+++ src/cocos/actions/Blink.hx
@@ -0,0 +1,11 @@
+package cocos.actions;
+
+@:native("cc.Blink")
+extern class Blink extends ActionInterval {
+    @:selfCall
+    public function new(duration: Float, blinkCount: Int);
+    public function initWithDuration(duration: Float, blinkCount: Int) : Bool;
+
+    public function clone() : Blink;
+    public function reverse() : Blink;
+}
